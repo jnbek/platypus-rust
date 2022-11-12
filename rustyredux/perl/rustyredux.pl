030f1ca2ad9d594622a7eaf7995fa5b900fa3a71
John D Jones III <jnbek1972@gmail.com>
2022-11-12 17:10:40 -0600

    Calling it a day

--- rustyredux/perl/rustyredux.pl
+++ rustyredux/perl/rustyredux.pl
@@ -1,35 +1,17 @@
 #!/usr/bin/env perl 
-#===============================================================================
-#
-#         FILE:  rustyredux.pl
-#
-#        USAGE:  ./rustyredux.pl
-#
-#  DESCRIPTION:
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  YOUR NAME (),
-#      COMPANY:
-#      VERSION:  1.0
-#      CREATED:  11/12/22 15:53:13
-#     REVISION:  ---
-#===============================================================================
 
 use strict;
 use warnings;
 use v5.24;
 use FindBin;
-use FFI::Platypus 2.00;
+use FFI::Platypus;
 use Data::Dumper;
 
 ( bless {}, __PACKAGE__ )->main();
 
-sub shared_lib  { "$FindBin::Bin/../target/release/librustyredux.so" }
+sub release_lib { "$FindBin::Bin/../target/release/librustyredux.so" }
 
-sub release_lib { "$FindBin::Bin/../target/debug/deps/librustyredux.so" }
+sub debug_lib { "$FindBin::Bin/../target/debug/deps/librustyredux.so" }
 
 sub ffi {
     my $self = shift;
@@ -42,8 +24,32 @@ sub ffi {
 
 sub main {
     my $self = shift;
+    my $ffi  = $self->ffi( $self->debug_lib );
+    $ffi->attach( make_point   => [ 'int',    'int' ]    => 'string' );
+    $ffi->attach( get_distance => [ 'string', 'string' ] => 'double' );
+    say "Test", make_point( 2, 2 );
 
 
     return 0;
 }
+__END__
+my $make_point = FFI::Raw->new(
+    $libpoints,
+    'make_point',
+    FFI::Raw::string,
+    FFI::Raw::int, FFI::Raw::int,
+);
+
+my $get_distance = FFI::Raw->new(
+    $libpoints,
+    'get_distance',
+    FFI::Raw::double,
+    FFI::Raw::string, FFI::Raw::string,
+);
+
+my $p1 = $make_point->call(2,2);
+my $p2 = $make_point->call(4,4);
+
+my $result = $get_distance->call($p1, $p2);
+say "The distance from (2,2) to (4,4) is $result (the square root of 8).";
 
