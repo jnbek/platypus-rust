3a25ebbeeb66b82cd576d8d808ddcdd13504835f
John D Jones III <jnbek1972@gmail.com>
2025-01-25 18:17:01 -0600

    Fixed the issues with the data types so it runs properly

--- rustyredux/perl/rustyredux.pl
+++ rustyredux/perl/rustyredux.pl
@@ -11,13 +11,13 @@ use Data::Dumper;
 
 sub release_lib { "$FindBin::Bin/../target/release/librustyredux.so" }
 
-sub debug_lib { "$FindBin::Bin/../target/debug/deps/librustyredux.so" }
+sub debug_lib { "$FindBin::Bin/../target/debug/librustyredux.so" }
 
 sub ffi {
     my $self = shift;
     my $lib  = shift;
     return $self->{'_ffi'} ||= FFI::Platypus->new(
-        api => 2,
+#        api => 2,
         lib => $lib,
     );
 }
@@ -25,10 +25,13 @@ sub ffi {
 sub main {
     my $self = shift;
     my $ffi  = $self->ffi( $self->debug_lib );
-    $ffi->attach( make_point   => [ 'int',    'int' ]    => 'string' );
-    $ffi->attach( get_distance => [ 'string', 'string' ] => 'double' );
-    say "Test", make_point( 2, 2 );
-
+    $ffi->attach( make_point   => [ 'int',    'int' ]    => 'opaque' );
+    $ffi->attach( get_distance => [ 'opaque', 'opaque' ] => 'double' );
+    #print Dumper($ffi);
+    my $p1 = make_point( 2, 2 );
+    my $p2 = make_point( 4, 4 );
+    my $result = get_distance($p1,$p2);
+    say "The distance from (2,2) to (4,4) is $result (the square root of 8).";
 
     return 0;
 }
